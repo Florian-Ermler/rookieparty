0e3d698926501fa59882168ad65cacc90285f10c
Florian-Ermler <Florian@Ermler.dev>
2022-07-20 16:39:35 +0200

    [UPDATE] login & register style + login logic

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UsernameService } from '../auth.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
-  constructor(private _router: Router, private _service: UsernameService) {}
+  constructor(private _router: Router, private _service: AuthService) {}
 
   username: string = '';
 
   async login() {
-    if (this.username !== '') {
-      if (await this._service.isUsernameUsed(this.username)) {
-        this._service.login(this.username);
-        this._router.navigate(['games']);
-      } else {
-        alert('Nutzer ist nicht bekannt');
-      }
+    if (this.username.trim() === '') return;
+    if (await this._service.isUsernameUsed(this.username)) {
+      this._service.login(this.username);
+      this._router.navigate(['games']);
     } else {
-      alert('Bitte einen Namen eintragen');
+      alert('Nutzer ist nicht bekannt');
     }
   }
 
